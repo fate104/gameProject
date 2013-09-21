40e12a9bbf7566e8b69e8aceb825694de67a5b54
chenquan <chenquanyj@sina.com>
2013-09-22 03:45:21 +0800

    123

---
+++ ui交互示例/Home_files/axurerp_pagespecificscript.js
@@ -0,0 +1,94 @@
+﻿for(var i = 0; i < 30; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u17'] = 'center';gv_vAlignTable['u29'] = 'top';gv_vAlignTable['u8'] = 'center';gv_vAlignTable['u21'] = 'top';gv_vAlignTable['u14'] = 'center';gv_vAlignTable['u4'] = 'center';gv_vAlignTable['u1'] = 'center';
+u10.style.cursor = 'pointer';
+$axure.eventManager.click('u10', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u12','','none',500);
+
+	SetPanelVisibility('u20','','none',500);
+
+	SetPanelVisibility('u26','','none',500);
+
+	SetPanelVisibility('u2','hidden','none',500);
+
+	SetPanelVisibility('u15','hidden','none',500);
+
+	SetPanelVisibility('u18','hidden','none',500);
+
+	SetPanelVisibility('u22','hidden','none',500);
+
+	SetPanelVisibility('u24','hidden','none',500);
+
+	SetPanelVisibility('u28','hidden','none',500);
+
+}
+});
+
+u11.style.cursor = 'pointer';
+$axure.eventManager.click('u11', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u15','','none',500);
+
+	SetPanelVisibility('u22','','none',500);
+
+	SetPanelVisibility('u28','','none',500);
+
+	SetPanelVisibility('u2','hidden','none',500);
+
+	SetPanelVisibility('u12','hidden','none',500);
+
+	SetPanelVisibility('u18','hidden','none',500);
+
+	SetPanelVisibility('u20','hidden','none',500);
+
+	SetPanelVisibility('u24','hidden','none',500);
+
+	SetPanelVisibility('u26','hidden','none',500);
+
+}
+});
+
+u9.style.cursor = 'pointer';
+$axure.eventManager.click('u9', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u2','','none',500);
+
+	SetPanelVisibility('u18','','none',500);
+
+	SetPanelVisibility('u24','','none',500);
+
+	SetPanelVisibility('u12','hidden','none',500);
+
+	SetPanelVisibility('u20','hidden','none',500);
+
+	SetPanelVisibility('u26','hidden','none',500);
+
+	SetPanelVisibility('u28','hidden','none',500);
+
+	SetPanelVisibility('u22','hidden','none',500);
+
+	SetPanelVisibility('u15','hidden','none',500);
+
+}
+});
+gv_vAlignTable['u27'] = 'top';gv_vAlignTable['u23'] = 'top';gv_vAlignTable['u25'] = 'top';gv_vAlignTable['u19'] = 'top';
+u5.style.cursor = 'pointer';
+$axure.eventManager.click('u5', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Page_1.html');
+
+}
+});
