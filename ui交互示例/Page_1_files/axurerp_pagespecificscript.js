fcdfad656e5d710b54affcba63efc4deb1b72a0f
chenquan <chenquanyj@sina.com>
2013-09-23 06:51:54 +0800

    123

--- ui交互示例/Page_1_files/axurerp_pagespecificscript.js
+++ ui交互示例/Page_1_files/axurerp_pagespecificscript.js
@@ -1,7 +1,24 @@
-﻿for(var i = 0; i < 2; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+﻿for(var i = 0; i < 48; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
 
 $axure.eventManager.pageLoad(
 function (e) {
 
 });
-gv_vAlignTable['u1'] = 'center';
+gv_vAlignTable['u17'] = 'center';gv_vAlignTable['u30'] = 'center';gv_vAlignTable['u21'] = 'center';gv_vAlignTable['u6'] = 'center';gv_vAlignTable['u15'] = 'center';gv_vAlignTable['u13'] = 'center';gv_vAlignTable['u4'] = 'center';gv_vAlignTable['u43'] = 'center';gv_vAlignTable['u40'] = 'center';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u37'] = 'center';gv_vAlignTable['u11'] = 'center';gv_vAlignTable['u9'] = 'center';gv_vAlignTable['u27'] = 'center';gv_vAlignTable['u24'] = 'center';document.getElementById('u46_img').tabIndex = 0;
+
+u46.style.cursor = 'pointer';
+$axure.eventManager.click('u46', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u25','toggle','none',500);
+
+	SetPanelVisibility('u41','toggle','none',500);
+
+	SetPanelVisibility('u22','toggle','none',500);
+
+	SetPanelVisibility('u7','toggle','none',500);
+
+}
+});
+gv_vAlignTable['u19'] = 'center';gv_vAlignTable['u47'] = 'center';gv_vAlignTable['u45'] = 'top';gv_vAlignTable['u33'] = 'center';
